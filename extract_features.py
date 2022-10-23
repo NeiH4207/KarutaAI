92ac8f72f4e44ab6c6cf81ae7670899ccc750d11
Vu Quoc Hien <hien.vu@genestory.ai>
2022-10-24 01:34:16 +0700

    fixed length

--- extract_features.py
+++ extract_features.py
@@ -1,23 +1,19 @@
 import argparse
 import os
-import numpy as np
-from src.data_helper import load_data, preprocess
-from models.lstm import CLSTM
-import torch
-from src.trainer import Trainer
+from src.data_helper import preprocess
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--train-folder', type=str,
-                        default='generated_data/train',
+                        default='generated_data/max10/train',
                         help='path to training data')
     
     parser.add_argument('--val-folder', type=str,
-                        default='generated_data/val',
+                        default='generated_data/max10/val',
                         
                         help='path to training data')
     parser.add_argument('--test-folder', type=str,
-                        default='generated_data/test',
+                        default='generated_data/max10/test',
                         help='path to training data')
     
     parser.add_argument('--original-label-data-path', type=str,
@@ -25,39 +21,14 @@ def parse_args():
                         help='')
     
     parser.add_argument('--processed-data-path', type=str,
-                        default='tmp',
+                        default='transformed/max10/',
                         help='')
-    
-    parser.add_argument('--model-path', type=str, default='./trainned_models"')
-    
-    parser.add_argument('-d', '--model-save-dir', type=str, 
-                        default='trainned_models/',
-                        help='directory to save model')
-    
-    # Model hyperparameters
-    parser.add_argument('-l', '--loss', type=str, default='bce',
-                        help='loss function to use')
-    
-    parser.add_argument('-o', '--optimizer', type=str, default='adam',
-                        help='optimizer to use')
-    
-    parser.add_argument('-e', '--epochs', type=int, default=1000,
-                        help='number of epochs to train')
-    
-    parser.add_argument('--lr', type=float, default=0.0001,  
-                        help='learning rate')
-    
-    parser.add_argument('-b', '--batch-size', type=int, default=32,
-                        help='batch size')
     
     parser.add_argument('--seed', type=int, default=233,
                         help='seed for training')
     
     parser.add_argument('--load-model', action='store_true',
                         help='Retrainging with old model')
-    
-    parser.add_argument('--preprocess', action='store_true', # default=True,
-                        help='Show validation results')
     
     parser.add_argument('-v', '--verbose', action='store_true',
                         help='Show validation results')
@@ -68,23 +39,20 @@ def parse_args():
 def main():
     args = parse_args()
     
-    
     data_config = {
         'batch-length': 32768,
         'num_mfcc': 64,
         'num_chroma': 64,
         'n_fft': 2048,
         'hop_length': 512,
-        'timeseries_length': 469,
-        'sr': 48000
+        'timeseries_length': 235,
+        'sr': 48000,
+        'fixed-time': 2.5
     }
     
-    preprocess(args.val_folder, 
-                os.path.join(args.processed_data_path, 'val'), data_config)
-    preprocess(args.train_folder, 
-                os.path.join(args.processed_data_path, 'train'), data_config)
-    preprocess(args.test_folder, 
-                os.path.join(args.processed_data_path, 'test'), data_config)
+    preprocess(args.val_folder, os.path.join(args.processed_data_path, 'val'), data_config)
+    preprocess(args.train_folder, os.path.join(args.processed_data_path, 'train'), data_config)
+    preprocess(args.test_folder, os.path.join(args.processed_data_path, 'test'), data_config)
     
 if __name__ == "__main__":
     main()
