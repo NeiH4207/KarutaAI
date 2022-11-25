2070f2c4348f2652bcd85482667e2b6f474f5ee9
hienvq <hien.vu@genestory.ai>
2022-11-25 23:28:42 +0700

    find offset

--- inference.py
+++ inference.py
@@ -1,15 +1,18 @@
 import argparse
 from models.arcnn import ARCNN
-from models.lstm import CLSTM
+from models.lstm import CLSTM, CLSTM2
+import numpy as np
+from tqdm import tqdm
 import torch
+import librosa
 from src.predictor import Predictor
-from src.trainer import Trainer
+from configs.conf import data_config
 
 def parse_args():
     parser = argparse.ArgumentParser()
     # Dataset directory
     parser.add_argument('-a', '--audio-file-path', type=str, 
-                        default='data/Q20.wav',
+                        default='data/Q_20_4_0.wav',
                         help='audio file to training data')
     
     parser.add_argument('--save-file-path', type=str, 
@@ -37,44 +40,70 @@ def main():
     device = "cuda" if torch.cuda.is_available() else "cpu"
     if args.cpu:
         device = 'cpu'
-    
-    data_config = {
-        'num_mfcc': 39,
-        'num_chroma': 64,
-        'n_fft': 2048,
-        'hop_length': 512,
-        'sr': 48000,
-        'fixed-time': 1
-    }
-    
-    data_config['timeseries_length'] = int(1 + \
-        (data_config['fixed-time'] * data_config['sr'] - 1) // data_config['hop_length'])
-
-    model = CLSTM(
+        
+    model = CLSTM2(
         input_size=128,
         hidden_size=512,
         num_layers=2,
         num_classes=88,
         device=device
     )
-    
-    # model = ARCNN(
-    #     input_shape=(data_config['timeseries_length'], 128),
-    #     num_chunks= 4,
-    #     in_channels=1,
-    #     rnn_hidden_size=512,
-    #     rnn_num_layers=2,
-    #     num_classes=88, 
-    #     device=device
-    # )
 
-    predictor = Predictor(model, device=device)
+    predictor = Predictor(model, data_config, fixed_length=False, device=device)
     predictor.load_model_from_path(args.model_file_path)
-    prob_out, labels = predictor.predict(args.audio_file_path, data_config,
-        k=88, plot=True, 
-        question_id=0, return_label=True,
-        save_path=None)
-    print(str(sorted(labels[prob_out.argsort()[::-1]][:args.k])).replace(' ', ', ').replace('\'', '\"'))
     
+    prob_out, labels = predictor.predict(args.audio_file_path, 
+        plot=True, 
+        return_label=True,
+        save_path=None)
+    orders = prob_out.argsort()[::-1][:args.k]
+    
+    print(str(labels[orders][:args.k]).replace('\'', '\"'))
+    
+    old_prob_out = prob_out
+    prob_audio, sr = librosa.load(args.audio_file_path, 
+                                    sr=data_config['sr'])
+    
+    selected_audio = []
+    selected_audio_idx = []
+    
+    for j, audio_name in enumerate(labels[orders[:-1]]):
+        file_path = 'data/JKspeech/{}.wav'.format(audio_name)
+        original_audio, sr = librosa.load(file_path, 
+                                      sr=data_config['sr'])
+        offset = 5
+        print(j, audio_name)
+        for i in range(min(1, len(original_audio)//offset)):
+            tmp_audio = original_audio[i*offset:i*offset+len(prob_audio)]
+            if i*offset + len(prob_audio) >= len(original_audio):
+                padding_size = i*offset+len(prob_audio)-len(original_audio)
+                tmp_audio = np.concatenate([tmp_audio, [0] * padding_size])
+            tmp_audio = prob_audio - tmp_audio
+            prob_out, labels = predictor.predict_by_audio(
+                tmp_audio[:48000], 
+                plot=False, 
+                return_label=True,
+                save_path=None)
+            print('{}/{}:'.format(i, len(original_audio)//offset), 
+                  np.round(prob_out[orders], 2))
+            if old_prob_out[orders[j]] - prob_out[orders[j]] > 0.5:
+                print('Offset {} in audio {}'.format(i, audio_name))
+                selected_audio.append(audio_name)
+                selected_audio_idx.append(j)
+                prob_audio = tmp_audio
+                predictor.plot_prob(prob_out, labels)
+                print(str(labels[orders]).replace('\'', '\"'))
+                break
+            
+    prob_out, labels = predictor.predict_by_audio(
+                prob_audio, 
+                plot=False, 
+                return_label=True,
+                save_path=None)
+    prob_out[np.array(selected_audio_idx)] = 0
+    orders = prob_out.argsort()[::-1][:args.k]
+    predictor.plot_prob(prob_out[orders], labels[orders])
+    selected_audio += labels[orders[:args.k - len(selected_audio_idx)]].tolist()
+    print(str(sorted(selected_audio)).replace('\'', '\"'))
 if __name__ == "__main__":
     main()
